6e180ef477e278297b9dd2aa6102b4c515fd6d38
Ryan Low <ryan_low@rp.edu.sg>
2023-05-04 10:14:40 +0800

    Completed up to Section B

--- app/src/main/java/sg/edu/rp/c346/id22000000/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22000000/week03/MainActivity.java
@@ -3,12 +3,24 @@ package sg.edu.rp.c346.id22000000.week03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Step 1 - declare variables
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
     }
 }
